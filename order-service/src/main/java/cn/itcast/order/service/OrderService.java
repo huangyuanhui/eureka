d5f832f0dc5603098d5beeed76275dafb3704e62
huangyuanhui <2278181701@qq.com>
2022-07-05 22:09:32 +0800

    服务调用：基于RestTemplate发起http请求实现远程调用问题： 此处将服务提供者的地址硬编码在代码中，是有很大问题的，一是在开发时。会有开发环境、测试环境、生产环境、每次环境变化，有待修改代码； 二是服务会做集群，同一服务的多个集群的地址是不一样的！ 总结： 1：服务消费者该如何获取服务提供者的地址信息？ 2：如果有服务提供者做了集群，消费者该如何选择？ 3：消费者如何得知服务提供者的健康状态？

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -20,6 +20,11 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2：利用RestTemplate发送http请求，查询用户
+        /**
+         * 此处将服务提供者的地址硬编码在代码中，是有很大问题的，一是在开发时。会有开发环境、测试环境、
+         * 生产环境、每次环境的变更服务的地址可能也会跟着变化，采用硬编码的方式，每次都要修改代码
+         * 二是服务会做集群，同一服务的多个集群的地址是不一样的！
+         */
         String url = "http://localhost:8081/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
