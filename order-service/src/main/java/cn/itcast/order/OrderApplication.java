ae9525b745a718972271157e746ea00eb8c4b6a3
huangyuanhui <2278181701@qq.com>
2022-07-06 20:05:10 +0800

    服务调用：eureka注册中心原理    eureka分为：    1：eureka注册中心    2：eureka客户端
    总结：
    1：服务消费者该如何获取服务提供者的地址信息？
    eureka客户端在启动时，会把自己的信息注册到eureka注册中心：比如服务名称、服务地址、服务IP；eureka注册中心会维护一个服务列表。
    
    2：如果有服务提供者做了集群，消费者该如何选择？
    当其他eureka客户端需要调用时，会从eureka注册中心拉取服务信息，当拉去成功后，会做负载均衡，根据服务名挑选出一个服务发起远程调用。
    
    3：消费者如何得知服务提供者的健康状态？
    eureka客户端默认每隔三十秒会香eureka注册中心发送一次心跳，确认自己的健康状态，也就是说如果eureka在固定时间内没有收到客户端的心跳，就会把该eureka客户端从服务列表中剔除，那么就可以感知到eureka客户端的健康状态。

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -15,6 +15,19 @@ public class OrderApplication {
     }
 
     /**
+     * eureka注册中心原理
+     * eureka分为：
+     * 1：eureka注册中心
+     * 2：eureka客户端
+     *  eureka客户端在启动时，会把自己的信息注册到eureka注册中心：比如服务名称、服务地址、服务IP
+     *  当其他eureka客户端需要调用时，会从eureka注册中心拉取服务信息
+     *  拉取成功后，后根据服务命去负载均衡选出一个服务实例进行调用
+     *
+     *  eureka客户端默认每隔三十秒会香eureka注册中心发送一次心跳，确认自己的健康状态，也就是说如果eureka在
+     *  固定时间内没有收到客户端的心跳，就会把该eureka客户端从服务列表中剔除，那么就可以感知到eureka客户端的健康状态
+     */
+
+    /**
      * 创建RestTemplate对象并注入容器
      * @return
      */
